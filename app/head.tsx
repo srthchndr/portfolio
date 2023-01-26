6d89e366f4dbbdf884cf12ac0d457c426af55cd9
Sarath Madala <srthchndr125@gmail.com>
2023-01-26 14:07:35 -0600

    Added Bing meta tag

--- app/head.tsx
+++ app/head.tsx
@@ -10,8 +10,12 @@ export default function Head() {
                 content={'Freelance developer looking to work with you and improve your product experience with my skills. I am proficient in building web applications using javascript frameworks.'}
                 name={'description'}
             />
+            <meta
+                content={'FDC6C1C96614B7485587D570553FD2EA'}
+                name={'msvalidate.01'}
+            />
             <link
-                href={'/favicon.ico'}
+                href={'/logo.png'}
                 rel={'icon'}
             />
         </>
