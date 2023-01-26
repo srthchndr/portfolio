e109562ede2d9cb97e736cb8d62bec9a93ebd0ed
Sarath Madala <srthchndr125@gmail.com>
2023-01-26 16:54:04 -0600

    Only embed tag

--- components/PDFViewerComponent/index.tsx
+++ components/PDFViewerComponent/index.tsx
@@ -19,18 +19,10 @@ export default function PDFViewerComponent() {
     const pdf = use(getPdfUrl());
 
     return (
-        <object
-            className={'w-full h-full md:w-[800px] md:h-[550px]'}
-            data={`${pdf.url}#toolbar=0&view=fitH`}
-            height={'100%'}
+        <embed
+            className={'w-full h-full md:w-[600px] md:h-[550px]'}
+            src={'https://drive.google.com/file/d/1gUYd0pvSpCsrrPpsp6CC6ZsKQm-18jSh/view?usp=sharing#toolbar=0&view=fitH'}
             type={'application/pdf'}
-            width={'100%'}
-        >
-            <embed
-                className={'w-full h-full md:w-[600px] md:h-[550px]'}
-                src={'https://drive.google.com/file/d/1gUYd0pvSpCsrrPpsp6CC6ZsKQm-18jSh/view?usp=sharing#toolbar=0&view=fitH'}
-                type={'application/pdf'}
-            />
-        </object>
+        />
     );
 }
