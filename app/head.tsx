cec805b1578e0b83835183e995fb8c84cbddf043
Sarath Madala <srthchndr125@gmail.com>
2023-01-26 14:45:00 -0600

    updated meta tags

--- app/head.tsx
+++ app/head.tsx
@@ -7,12 +7,68 @@ export default function Head() {
                 name={'viewport'}
             />
             <meta
-                content={'Freelance developer looking to work with you and improve your product experience with my skills. I am proficient in building web applications using javascript frameworks.'}
+                content={'sarath, sarath madala, freelancer, freelancing, portfolio'}
+                name={'keywords'}
+            />
+            <meta
+                content={'Discover my developer portfolio website optimized for SEO, showcasing my skills, work experience, and personal bio in a clean and responsive design. Browse through my projects, filter by category, and find my contact information and social media links all in one place. Let\'s build something great together!'}
                 name={'description'}
             />
             <meta
                 content={'FDC6C1C96614B7485587D570553FD2EA'}
                 name={'msvalidate.01'}
+            />
+            <meta
+                content={'Sarath Madala'}
+                name={'creator'}
+            />
+            <meta
+                content={'index follow'}
+                name={'robots'}
+            />
+            <meta
+                content={'dark light'}
+                name={'color-scheme'}
+            />
+            <meta
+                content={'Sarath Madala'}
+                property={'og:title'}
+            />
+            <meta
+                content={'website'}
+                property={'og:type'}
+            />
+            <meta
+                content={'https://www.sarathmadala.com'}
+                property={'og:url'}
+            />
+            <meta
+                content={'https://www.sarathmadala.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogo.8cb6c954.png&w=128&q=75'}
+                property={'og:image'}
+            />
+            <meta
+                content={"Discover my developer portfolio website optimized for SEO, showcasing my skills, work experience, and personal bio in a clean and responsive design. Browse through my projects, filter by category, and find my contact information and social media links all in one place. Let\\'s build something great together!"}
+                property={'og:description'}
+            />
+            <meta
+                content={'summary'}
+                name={'twitter:card'}
+            />
+            <meta
+                content={'Sarath Madala'}
+                name={'twitter:title'}
+            />
+            <meta
+                content={'https://www.sarathmadala.com'}
+                name={'twitter:url'}
+            />
+            <meta
+                content={'https://www.sarathmadala.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogo.8cb6c954.png&w=128&q=75'}
+                name={'twitter:image'}
+            />
+            <meta
+                content={"Discover my developer portfolio website optimized for SEO, showcasing my skills, work experience, and personal bio in a clean and responsive design. Browse through my projects, filter by category, and find my contact information and social media links all in one place. Let\\\\'s build something great together!"}
+                name={'twitter:description'}
             />
             <link
                 href={'/logo.png'}
