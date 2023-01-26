3bd8843d0d946cffea2db8b9ab7cdea380b259da
Sarath Madala <srthchndr125@gmail.com>
2023-01-26 16:57:20 -0600

    Embed with google doc link

--- components/PDFViewerComponent/index.tsx
+++ components/PDFViewerComponent/index.tsx
@@ -1,26 +1,36 @@
-import React from 'react';
+import React, {
+    use
+} from 'react';
 
-// import {
-//     sanityClient
-// } from '../../sanity';
+import {
+    sanityClient
+} from '../../sanity';
 
-// async function getPdfUrl() {
-//     const query = `*[_type=='pdf'] {
-//         'url': pdf.asset->url
-//       }[0]`;
+async function getPdfUrl() {
+    const query = `*[_type=='pdf'] {
+        'url': pdf.asset->url
+      }[0]`;
 
-//     const result = await sanityClient.fetch(query);
-//     return result;
-// }
+    const result = await sanityClient.fetch(query);
+    return result;
+}
 
 export default function PDFViewerComponent() {
-    // const pdf = use(getPdfUrl());
+    const pdf = use(getPdfUrl());
 
     return (
-        <embed
-            className={'w-full h-full md:w-[600px] md:h-[550px]'}
-            src={'https://drive.google.com/file/d/1gUYd0pvSpCsrrPpsp6CC6ZsKQm-18jSh/view?usp=sharing#toolbar=0&view=fitH'}
+        <object
+            className={'w-full h-full md:w-[800px] md:h-[550px]'}
+            data={`${pdf.url}#toolbar=0&view=fitH`}
+            height={'100%'}
             type={'application/pdf'}
-        />
+            width={'100%'}
+        >
+            <embed
+                className={'w-full h-full md:w-[600px] md:h-[550px]'}
+                src={`${pdf.url}#toolbar=0&view=fitH`}
+                type={'application/pdf'}
+            />
+        </object>
     );
 }
